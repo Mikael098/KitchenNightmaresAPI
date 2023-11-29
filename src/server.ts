f712ddf02b5bc1f35065427d94c63fc53a476c1a
Mikael Fortier <mr_snake@outlook.fr>
2023-11-29 14:24:35 -0500

    MiseEnLigneDocumentation

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,19 @@ const app = express();
 
 // **** Setup **** //
 
+// rend disponible la documentation de l'interface logicielle
+app.get('/api-docs/', async (req, res) => {
+  res.set('Content-Security-Policy', 'script-src blob:');
+  res.set('Content-Security-Policy', 'worker-src blob:');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// redirige vers api-docs
+app.get('/', (req, res) => {
+  res.redirect('/api-docs');
+});
+
+
 // Basic middleware
 app.use(express.json());
 
