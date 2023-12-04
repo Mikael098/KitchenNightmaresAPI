2ae4f165dd9a6e53bdc784063fd77dffe2be146c
Mikael Fortier <mr_snake@outlook.fr>
2023-12-04 15:22:36 -0500

    Suppression de EnvVars

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import './pre-start'; // Must be the first import
 import logger from 'jet-logger';
 import { connect } from 'mongoose';
-import EnvVars from './constants/EnvVars';
+//import EnvVars from './constants/EnvVars';
 import server from './server';
 import http  from 'http';
 
 
 // **** Run **** //
 const SERVER_START_MSG =
-  'Express server started on port: ' + EnvVars.Port.toString();
-    server.listen(EnvVars.Port, () => {
-      console.log('Serveur écoute sur le port ' + EnvVars.Port);
+  //'Express server started on port: ' + process.env.PORT.toString();
+    server.listen(process.env.PORT, () => {
+      console.log('Serveur écoute sur le port ' + process.env.PORT);
       logger.info(SERVER_START_MSG);
     });
     console.log('Connecté MongoDB');
